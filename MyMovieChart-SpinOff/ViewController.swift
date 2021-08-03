979f1c438237971cd621a8e28ab00932eaa70313
ujeon <wj9304@gmail.com>
2021-08-03 11:11:15 +0900

    Initial Commit

---
+++ MyMovieChart-SpinOff/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyMovieChart-SpinOff
+//
+//  Created by 우전 on 2021/08/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
